4ebb91d5a65dfd28de8122f52975f85e28fa0925
Samani G. Gikandi <samani@gojulas.com>
2020-06-08 21:14:19 -0700

    Updates Capabilities implementation
    * Capaiblity args are not ordered so we use a HashSet to get
      constant-time lookups.
    * The capabilities iterator returns Capability types instead of tuples
      for better ergonomics

--- src/types/response/capabilities.rs
+++ src/types/response/capabilities.rs
@@ -4,23 +4,30 @@ use std::convert::TryFrom;
 use crate::error::{Error, Result};
 use crate::types::prelude::*;
 use crate::types::response::util::err_if_not_kind;
+use nom::lib::std::collections::HashSet;
 
 /// Server capabilities
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Capabilities(HashMap<String, Option<Vec<String>>>);
+pub struct Capabilities(HashMap<String, Capability>);
 
-// FIXME(craft): newtype (or deprecate) Capabilities iterator
+/// A capability advertised by the server
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Capability {
+    pub name: String,
+    pub args: Option<HashSet<String>>
+}
+
 impl Capabilities {
 
     /// An iterator over the capabilities
     pub fn iter(&self) -> Iter<'_>{
         Iter {
-            inner: self.0.iter()
+            inner: self.0.values()
         }
     }
 
     /// Retrieve a capability if it exists
-    pub fn get(&self, key: impl AsRef<str>) -> Option<&Option<Vec<String>>> {
+    pub fn get(&self, key: impl AsRef<str>) -> Option<&Capability> {
         self.0.get(key.as_ref())
     }
 }
@@ -28,11 +35,11 @@ impl Capabilities {
 /// Created by [`Capabilities::iter`]
 #[derive(Clone, Debug)]
 pub struct Iter<'a> {
-    inner: hash_map::Iter<'a, String, Option<Vec<String>>>,
+    inner: hash_map::Values<'a, String, Capability>,
 }
 
 impl<'a> Iterator for Iter<'a> {
-    type Item = (&'a String, &'a Option<Vec<String>>);
+    type Item = &'a Capability;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.inner.next()
@@ -55,7 +62,7 @@ impl TryFrom<&RawResponse> for Capabilities {
             .ok_or_else(|| Error::de("Missing data blocks."))
             .map(DataBlocks::lines)?;
 
-        let capabilities: HashMap<String, Option<Vec<String>>> = db_iter
+        let capabilities: HashMap<String, Capability> = db_iter
             .map(String::from_utf8_lossy)
             .map(|entry| {
                 let mut entry_iter = entry.split_whitespace().peekable();
@@ -65,11 +72,17 @@ impl TryFrom<&RawResponse> for Capabilities {
                     .ok_or_else(|| Error::de("Entry does not have a label"))?;
 
                 let args = if entry_iter.peek().is_some() {
-                    Some(entry_iter.map(ToString::to_string).collect::<Vec<_>>())
+                    Some(entry_iter.map(ToString::to_string).collect::<HashSet<_>>())
                 } else {
                     None
                 };
-                Ok((label, args))
+
+                let cap = Capability {
+                    name: label.clone(),
+                    args
+                };
+
+                Ok((label, cap))
             })
             .collect::<Result<_>>()?;
 
